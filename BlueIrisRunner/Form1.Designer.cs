a8f59f91272a9ba18a0a1ae8bfa7110b047c3708
Pavel Fedoruk <pilotcode@yandex.ru>
2022-03-29 21:10:51 +0300

    Добавьте файлы проекта.

---
+++ BlueIrisRunner/Form1.Designer.cs
@@ -0,0 +1,171 @@
+﻿namespace BlueIrisRunner
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.processStatus = new System.Windows.Forms.Label();
+            this.asAdminCheck = new System.Windows.Forms.CheckBox();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(11, 12);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(186, 20);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Process Runner Checker";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 42);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(158, 26);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Checks if the program is running\r\nand run it if not";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(15, 213);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(278, 31);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Okay, lets go";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(12, 164);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(137, 13);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "Executable file not selected";
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 2000;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(15, 103);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(278, 20);
+            this.textBox1.TabIndex = 4;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(12, 87);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(74, 13);
+            this.label4.TabIndex = 5;
+            this.label4.Text = "Process name";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.ForeColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.label5.Location = new System.Drawing.Point(85, 87);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(80, 13);
+            this.label5.TabIndex = 6;
+            this.label5.Text = "for ex. Notepad";
+            // 
+            // processStatus
+            // 
+            this.processStatus.AutoSize = true;
+            this.processStatus.Location = new System.Drawing.Point(12, 126);
+            this.processStatus.Name = "processStatus";
+            this.processStatus.Size = new System.Drawing.Size(0, 13);
+            this.processStatus.TabIndex = 7;
+            // 
+            // asAdminCheck
+            // 
+            this.asAdminCheck.AutoSize = true;
+            this.asAdminCheck.Location = new System.Drawing.Point(15, 186);
+            this.asAdminCheck.Name = "asAdminCheck";
+            this.asAdminCheck.Size = new System.Drawing.Size(99, 17);
+            this.asAdminCheck.TabIndex = 8;
+            this.asAdminCheck.Text = "as administrator";
+            this.asAdminCheck.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(305, 256);
+            this.Controls.Add(this.asAdminCheck);
+            this.Controls.Add(this.processStatus);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Name = "Form1";
+            this.Text = "Process Runner Checker";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label processStatus;
+        private System.Windows.Forms.CheckBox asAdminCheck;
+    }
+}
+
---
+++ BlueIrisRunner/Form1.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace BlueIrisRunner
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        public string filePath;
+        public string fileName;
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+             
+        }
+
+        private bool CheckProcessRunning()
+        {
+            try
+            {
+                foreach (System.Diagnostics.Process p in System.Diagnostics.Process.GetProcesses())
+                {
+                    if (p.ProcessName.ToString() == fileName) return true;
+                }
+                return false;
+            }
+            catch (Exception ex)
+            {
+                return false;
+            }
+        }
+
+        private void OpenExecutableFile()
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            openFileDialog.ShowDialog();
+            filePath = openFileDialog.FileName;
+            label3.Text = openFileDialog.SafeFileName;
+        }
+
+        private void RunFile(bool asAdmin)
+        {
+            try
+            {
+                Process proc = new Process();
+                proc.StartInfo.FileName = filePath;
+                proc.StartInfo.UseShellExecute = true;
+                if (asAdmin) proc.StartInfo.Verb = "runas";
+                proc.Start();
+            }
+            catch(Exception ex)
+            {
+                timer1.Enabled = false;
+                timer1.Stop();
+                MessageBox.Show("Error while opening file:\n" + ex.Message);
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            OpenExecutableFile();
+            timer1.Enabled = true;
+            timer1.Start();
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            if (!CheckProcessRunning()) { RunFile(asAdminCheck.Checked); }
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            fileName = textBox1.Text;
+            if (CheckProcessRunning())
+            {
+                processStatus.Text = "process found";
+                processStatus.ForeColor = Color.Green;
+            }
+            else
+            {
+                processStatus.Text = "process not found";
+                processStatus.ForeColor = Color.Red;
+            }
+        }
+    }
+}
